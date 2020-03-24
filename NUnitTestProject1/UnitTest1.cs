1de87ec50b11143033512456a523809afd3f27ab
Samrjeet0007 <samarjeetkumar962@gmail.com>
2020-03-24 22:21:57 +0530

    commit

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+namespace Tests
+{
+    public class Tests
+    { 
+        [Test]
+        public void Test1()
+        {
+            int a = 5, b;
+            b = a;
+            Assert.AreEqual(a, b);
+        }
+    }
+}
